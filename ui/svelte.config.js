c720fadd2bce93dbcaa881588c2dce9b54beb6e1
D-Nice <d-nice2@protonmail.com>
2021-10-06 18:40:47 -0400

    chore(ui): rm mdsvex
    seems useless for our purposes after trying it

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,10 +1,7 @@
 import preprocess from "svelte-preprocess";
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
 
 const config = {
-  "extensions": [".svelte", ...mdsvexConfig.extensions],
-  preprocess: [mdsvex(mdsvexConfig), preprocess({
+  preprocess: [preprocess({
     postcss: true
   })]
 };
