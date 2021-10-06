a0e1b75f916a7a55ba5f71e08b085955deab7567
D-Nice <d-nice2@protonmail.com>
2021-10-06 19:59:13 -0400

    feat(ui): add initial mock ui

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,67 +1,15 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Navbar from './lib/Navbar.svelte'
+  import ViewNFT from './lib/ViewNFT.svelte'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-  <button class="btn btn-primary">daisyUI Button</button>
-
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div class='grid grid-cols-1 gap-6 lg:p-10 xl:grid-cols-3 lg:bg-base-200 rounded-box'>
+    <Navbar />
+    <ViewNFT />
+    <ViewNFT />
+    <ViewNFT />
+    <ViewNFT />
+    <ViewNFT />
+  </div>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
---
+++ ui/src/lib/ViewNFT.svelte
@@ -0,0 +1,33 @@
+<script>
+  // TODO improve paths so they don't have to be relative, but declarative
+  import nftImg1 from '../assets/cpunk-mock-nft.jpg'
+  import nftImg2 from '../assets/bape-mock-nft.jpg'
+  import nftImg3 from '../assets/mona-mock-nft.jpg'
+
+const nftImgs = [
+  nftImg1
+  , nftImg2
+  , nftImg3
+]
+</script>
+
+<div class="card row-span-3 shadow-lg compact space-y-20 bg-base-100">
+  <figure>
+    <img style="max-height: 452px" src={nftImgs[Math.random()*3 | 0]} alt='nft' />
+  </figure>
+  <div class="flex-row items-center space-x-4 card-body">
+    <div class="space-y-2">
+      <h2 class="card-title">
+        NFT Name
+      </h2>
+      <p class="text-base-content text-opacity-40">Collection: 0xDeaDbeEfDeaDbeEfDeaDbeEfDeaDbeEfDeaDbeEf</p>
+      <span class="font-mono text-xl countdown">
+        <span style="--value:23;"></span>h
+        <span style="--value:59;"></span>m
+        <span style="--value:59;"></span>s
+      </span>
+      <button class='btn btn-primary place-self-center w-full'>React</button>
+    </div>
+  </div>
+</div>
+
